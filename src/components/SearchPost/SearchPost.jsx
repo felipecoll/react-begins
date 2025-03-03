f2bcf50460383514d5d5ec05dc6480472ae0b59f
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-03 08:51:20 -0300

    Llamadas a api con dependencias en useEffect

--- src/components/SearchPost/SearchPost.jsx
+++ src/components/SearchPost/SearchPost.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 const SearchPost = () => {
 
     const [query, setQuery] = useState('')
-    const [post, setPost] = useState('')
+    const [posts, setPost] = useState([])
 
+    useEffect(() => {
+              fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${query}`)
+                .then(response => response.json())
+                .then(data => setPost(data))
+                .catch(error => console.error(error))
+            }, [query])
+        
   return (
-    <div>SearchPost</div>
+    <div>
+        <h1>Listado de post</h1>
+
+        <input type="text" 
+            value={query} 
+            onChange={(e) => setQuery(e.target.value)} 
+        />
+        <ul>
+            {
+            posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))
+            }
+        </ul>
+
+
+    </div>
   )
 }
 
